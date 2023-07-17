97bae94cf045cf5b5581785ea5e51406f586b4ab
Andrey <han@list.ru>
2023-07-17 22:27:07 +0300

    the result of homework

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,6 @@
 #include "example.h"
-
+#include "move_me_out.h"
 #include <iostream>
-
-void todo_move_me_out() {
-	std::cout << "This function should be located in the separated source file" << std::endl;
-}
 
 int main() {
 	std::cout << "Hello, World!" << std::endl;
