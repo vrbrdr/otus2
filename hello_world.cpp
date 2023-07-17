10ead6dbdd23c93047d2e4956b63495fdda1c9ad
Andrey <han@list.ru>
2023-07-17 22:10:44 +0300

    rollback to the original sources

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,13 +6,9 @@ void todo_move_me_out() {
 	std::cout << "This function should be located in the separated source file" << std::endl;
 }
 
-using std::cout;
-
 int main() {
 	std::cout << "Hello, World!" << std::endl;
 
-	int a{1}, b{2}, c{3};
-	cout << a << ", " << "b, " << "c";
 	example_function();
 	todo_move_me_out();
 	
